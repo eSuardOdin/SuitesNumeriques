3d66cb83f25d142ff9a2278c8b980c8e7767d532
eSuardOdin <esuardodin@gmail.com>
2023-10-18 22:45:03 +0200

    working.

--- testApp/SuitesConsole/Program.cs
+++ testApp/SuitesConsole/Program.cs
@@ -5,3 +5,4 @@ Console.WriteLine(ari.GetSum(10)); */
 Geometrique geo = new(2,1);
 //for (int i = 0; i < 10; i++) Console.WriteLine(geo.GetValueAt(i));
 Console.WriteLine(geo.GetSum(4));
+Console.WriteLine(geo.GetType());
--- testApp/SuitesConsole/class/Exercice.cs
+++ testApp/SuitesConsole/class/Exercice.cs
@@ -1,36 +1,52 @@
-public class Exercice
+public abstract class Exercice
 {
     private byte TypeExo {get; set;}
     public string Enonce {get; private set;}
+    public string Question {get; private set;}
     public string Reponse {get; private set;}
     private Suite SuiteExo {get; set;}
 
     public Exercice(byte typeExo)
     {
         TypeExo = typeExo;
+        GetNewSuite();
     }
+
+    public virtual void SetEnonce(string typeSuite)
+    {
+        Random rand = new();
+        Enonce = $"Soit la suite {typeSuite} (Un)";
+
+        // Si on a donne le premier terme et la raison
+        if(TypeExo == 1 || TypeExo == 3 || TypeExo == 5 || TypeExo == 6)
+        {
+            Enonce += $"de premier terme U0 et de raison r :\n\n\tU0: {SuiteExo.PremierTerme}\n\n\tr: {SuiteExo.Raison}";
+        }
+    }
+
 
 
     /// <summary>
     /// Sert à (re)génèrer l'exercice
     /// </summary>
     /// <param name="typeSuite"></param>
-    public void GetNewSuite(string typeSuite)
-    {
+    public abstract void GetNewSuite(string typeSuite);
+
+    {/* 
         // Attention, ce n'est pas ici qu'on met à jour les infos
         // de l'exercice mais ce sera handle par la GUI
         Random rand = new();
         // Géométrique
-        if(typeSuite == "geo")
+        if(typeSuite == "géométrique")
         {
             SuiteExo = new Geometrique(1,1);
         }
         // Arithmétique
-        else if (typeSuite == "ari")
+        else if (typeSuite == "arithmétique")
         {
             SuiteExo = new Arithmethique(1,1);
         }
         // Quelconque
         this.SuiteExo = new Suite(1,1);
-    }
+    } */
 }
