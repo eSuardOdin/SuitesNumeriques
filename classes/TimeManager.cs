41fecf9c5df6c5e6894a44039d825666a6437611
eSuardOdin <esuardodin@gmail.com>
2023-11-15 05:36:35 +0100

    Début du travail sur timer

--- classes/TimeManager.cs
+++ classes/TimeManager.cs
@@ -7,7 +7,7 @@ public class TimeManager
     // Timer d'une seconde
     System.Timers.Timer MyTimer = new(1000);
     public int SecondsLeft {get; private set;}
-
+    public event Action<int> SecondsLeftChanged;
     /// <summary>
     /// Constructeur de TimeManager, on définit l'intervalle entre les evenements, on s'abonne à l'evenement, on le déclenche en continu (AutoReset = true) et on l'active   
     /// </summary>
@@ -27,5 +27,7 @@ public class TimeManager
     private void OnSecEvent(Object source, ElapsedEventArgs e) 
     {
         SecondsLeft -=1;
+        // On déclenche l'event
+        SecondsLeftChanged.Invoke(SecondsLeft);
     }
 }
