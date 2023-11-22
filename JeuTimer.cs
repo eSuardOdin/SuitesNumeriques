90664dfa1fef811ee916c620adff660ccbf23e94
eSuardOdin <esuardodin@gmail.com>
2023-11-22 05:13:11 +0100

    Doc de JeuTimer.cs

--- JeuTimer.cs
+++ JeuTimer.cs
@@ -10,10 +10,24 @@ using System.Windows.Forms;
 
 namespace SuitesNumeriques
 {
+    /// <summary>
+    /// Classe gérant le formulaire de jeu avec contrainte
+    /// </summary>
     public partial class JeuTimer : Jeu
     {
+        /// <summary>
+        /// Instance de la classe gérant le timer et ses events
+        /// </summary>
         private TimeManager MyTimeManager { get; set; }
         
+        /// <summary>
+        /// Constructeur de la classe
+        /// </summary>
+        /// <param name="j1">Joueur 1</param>
+        /// <param name="j2">Joueur 2</param>
+        /// <param name="typePartie">Type de la partie (arithmétique, géométrique ou quelconque si implémentée)</param>
+        /// <param name="mainForm">Le formulaire parent</param>
+        /// <param name="tm">Le time manager</param>
         public JeuTimer(Player j1, Player j2, string typePartie, MainForm mainForm, TimeManager tm) : base(j1, j2, typePartie, mainForm)
         {
             InitializeComponent();
@@ -23,6 +37,10 @@ namespace SuitesNumeriques
             MyTimeManager.SecondsLeftChanged += UpdateTimeLabel;
         }
 
+        /// <summary>
+        /// Update du label indiquant le temps restant
+        /// </summary>
+        /// <param name="secondsLeft">Le nombre de secondes restantes à afficher</param>
         private void UpdateTimeLabel(int secondsLeft)
         {
             if (timeLbl.InvokeRequired && this != null)
@@ -31,32 +49,40 @@ namespace SuitesNumeriques
                 if (secondsLeft / 60 == 0) timeLbl.ForeColor = Color.Red;
                 else timeLbl.ForeColor = Color.Green;
             }
-            /*else
+            else
             {
                 timeLbl.Text = secondsLeft.ToString();
-                //timeLbl.Text = $"{(secondsLeft/60).ToString()}min {(secondsLeft%60).ToString()}sec";
-            }*/
+                timeLbl.Text = $"{(secondsLeft/60).ToString()}min {(secondsLeft%60).ToString()}sec";
+                if (secondsLeft / 60 == 0) timeLbl.ForeColor = Color.Red;
+                else timeLbl.ForeColor = Color.Green;
+            }
         }
 
 
+        /// <summary>
+        /// Override base.EndGame() pour que FinPartie écrive les meilleurs scores
+        /// </summary>
         protected override void EndGame()
         {
             // Fin de partie timée
             FinPartie fin = new(J1, J2, Versus.TypeSuite, mainForm, true);
             fin.Show();
-
             this.Dispose();
         }
 
 
         /// <summary>
-        /// Utilisé pour relancer le timer 
+        /// Utilisé pour relancer le timer depuis le formulaire de règles
         /// </summary>
         public void EnableTimer() => MyTimeManager.MyTimer.Start();
         
 
-
+        
+        
+        //--------------------------
         // EVENTS
+        //--------------------------
+
 
         /// <summary>
         /// Click du bouton valider
@@ -66,7 +92,8 @@ namespace SuitesNumeriques
         protected override void validBtn_Click(object sender, EventArgs e)
         {
             Player currentPlayer = IsFirstPlayer ? J1 : J2;
-            // Si on est pas dans la dernière question :
+            
+            // Si on est pas dans la dernière question on ne prend pas en compte les radio buttons:
             if(IndexExercice != 5)
             {
                 if (currentPlayer.Repondre(Versus.Exercices[IndexExercice], base.getRepTxtBox(false)))
@@ -84,25 +111,40 @@ namespace SuitesNumeriques
                 }
             }
             
-
+            // Reset du timer
             MyTimeManager.ResetTimer();
             base.validBtn_Click(sender, e);
         }
 
 
+        /// <summary>
+        /// Mets à jour le timer puis base.Event()
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected override void skipBtn_Click(object sender, EventArgs e)
         {
             UpdateTimeLabel(MyTimeManager.SecondsLeft);
             base.skipBtn_Click(sender, e);
         }
 
-
+        /// <summary>
+        /// Désactive le timer et affiche les règles 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected override void reglesBtn_Click(object sender, EventArgs e)
         {
             MyTimeManager.MyTimer.Stop();
             base.reglesBtn_Click(sender, e);
         }
 
+        /// <summary>
+        /// On ferme le jeu et réactive le menu principal. Attention : Peut provoquer des null ref quand le timer cherche à mettre
+        /// à jour le label qui a déjà été dispose. Je n'arrive pas encore à gérer les cas où ça arrive..
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected override void Jeu_FormClosed(object sender, FormClosedEventArgs e)
         {
             // Pour eviter la null reference de l'event qui ne peut pas mettre à jour le form
